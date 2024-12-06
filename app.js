8ccd1efc81b0fb5efe03f16ae1ace5c1b0afdc81
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-12-05 19:10:03 -0600

    Initial Configurations and App and server added

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
+
+const globalErrorHandler = require('./utils/appError');
+
+const AppError = require('./utils/appError');
+
+const app = express();
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+  }),
+);
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(express.static('public'));
+
+app.use(compression());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+app.use('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
